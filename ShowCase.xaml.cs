3280a1dccb3e61051bafa5f357af187d4cca8f52
Nomansky47 <kita31200@gmail.com>
2023-04-14 11:23:12 +0300

    Initial commit

---
+++ ShowCase.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cursed
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class ShowCase : Window
+    {
+        public ShowCase()
+        {
+            InitializeComponent();
+            Dgrid.ItemsSource=AirEntities.GetContext().Passengers.ToList();
+        }
+        private void EditClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("i forgor -_-");
+        }
+    }
+}
