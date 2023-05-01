4697f554ad1a0085ae5ad8d76db89f5d8ccff41e
Nomansky47 <kita31200@gmail.com>
2023-05-01 17:03:19 +0300

    finally(not)

--- PlanePage.xaml.cs
+++ PlanePage.xaml.cs
@@ -1,69 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 namespace Cursed
 {
-    /// <summary>
-    /// Логика взаимодействия для TicketPage.xaml
-    /// </summary>
     public partial class PlanePage : Page
     {
-        Flights flight=new Flights();
-        private void ButtonsAdd(int j,Panel panel, List<Tickets> tickets)
+        Flights _flight=new Flights();
+        Aircrafts _aircrafts= new Aircrafts();
+        private void ButtonsAdd(int j,Panel panel, List<Tickets> tickets, int max) //на вход подаются параметры: ряд, панель для добавления на экран,
+                                                                                   //таблица с билетами для проверки, и кол-во рядов в самолете
         {
-            int width = 50;
-            int height = 50;
-            Button[] button = new Button[4];
-            //ПОДПИСАТЬ МЕСТА ПО ТИПУ 11 21 и тд
-            for (int i = 1; i < 4; i++)
+            int width =40 ,height = 40;
+            for (int i = 1; i < max+1; i++)
             {
-                button[i] = new Button();
-                button[i].Name = "b" + j.ToString() + i.ToString();
-                if (tickets.Exists(p => p.FlightID==flight.FlightID&&p.Row ==j && p.Seat == i&&p.Userlogin==Navigator.login))
+                Button button = new Button();
+                button.Name = "b" + j.ToString() + i.ToString(); //наименование для кнопки, чтобы в будущем определять ряд и номер места
+                button.Content = j.ToString() + " "+ i.ToString(); //отображение ряда и места
+                if (tickets.Exists(p => p.FlightID==_flight.FlightID&&p.Row ==j && p.Seat == i&&p.Userlogin==Navigator.login))
                 {
-                    button[i].Background = Brushes.Red;
+                    button.Background = Brushes.Red; //если в базе данных уже куплен билет, то повторно купить не получится
                 }
-                else button[i].Click += ButtonOnClick;
-                button[i].Width = width;
-                button[i].Height = height;
-                panel.Children.Add(button[i]);
-                //КОмментарии для кода
-                //ПОЯСНТИЛЕЛЬНАЯ ЗАПИСКА
+                else button.Click += ButtonOnClick; //иначе можно купить
+                button.Width = width;
+                button.Height = height;
+                panel.Children.Add(button); // добавление кнопки на панель
+                //ПОЯСНИТЕЛЬНАЯ ЗАПИСКА 
             }
         }
         public PlanePage(Flights _selectedFlight)
         {
             InitializeComponent();
-            flight=_selectedFlight;
+            _flight=_selectedFlight;
             DataContext = _selectedFlight;
             List<Tickets> tickets= AirEntities.GetContext().Tickets.ToList();
-            ButtonsAdd(1,Panel1,tickets);
-            ButtonsAdd(2, Panel2, tickets);
-            ButtonsAdd(3, Panel3, tickets);
-            ButtonsAdd(4, Panel4, tickets);
-            ButtonsAdd(5, Panel5, tickets);
+            _aircrafts= AirEntities.GetContext().Aircrafts.FirstOrDefault(p => p.AircraftID == _flight.AircraftID);
+            int max = _aircrafts.NumberOfSeats/10;
+            ButtonsAdd(1, Panel3, tickets,max);
+            ButtonsAdd(2, Panel4, tickets, max);
+            ButtonsAdd(3, Panel5, tickets, max);
+            ButtonsAdd(4, Panel6, tickets, max); 
+            ButtonsAdd(5, Panel7, tickets, max);
+            ButtonsAdd(6, Panel8, tickets, max);
+            ButtonsAdd(7, Panel9, tickets, max);
+            ButtonsAdd(8, Panel10, tickets, max);
+            ButtonsAdd(9, Panel11, tickets, max);
+            ButtonsAdd(10, Panel12, tickets, max);
         }
         private void ButtonOnClick(object sender, EventArgs eventArgs)
         {
             Button button = (Button)sender;
             button.Click -= ButtonOnClick;
+            button.Background = Brushes.Red;
             Tickets ticket = new Tickets();
-            // if (тип самолета ==бизнес) цена= 300
-            Aircrafts aircrafts = AirEntities.GetContext().Aircrafts.FirstOrDefault(p=>p.AircraftID==flight.AircraftID);
-          switch (aircrafts.PlaneType)
-                {
+            switch (_aircrafts.PlaneType)
+            {
                 case "эконом":
                     ticket.Price = 2000;
                     break;
@@ -74,17 +66,22 @@ namespace Cursed
                     ticket.Price = 5000;
                     break;
             }
-            ticket.FlightID = flight.FlightID;
-            ticket.Row = int.Parse(button.Name[1].ToString());
-            ticket.Seat = int.Parse(button.Name[2].ToString());
-            ticket.Departure_Time = flight.Departure_Time;
+            ticket.FlightID = _flight.FlightID;
+            if (button.Name.Length==3)
+            {
+                ticket.Row = int.Parse(button.Name[1].ToString());
+                ticket.Seat = int.Parse(button.Name[2].ToString());
+            
+            }
+            else
+            {
+                ticket.Row = int.Parse(button.Name[1].ToString() + button.Name[2]);
+                ticket.Seat = int.Parse(button.Name[3].ToString());
+            }
+            ticket.Departure_Time = _flight.Departure_Time;
             ticket.Userlogin = Navigator.login;
             AirEntities.GetContext().Tickets.Add(ticket);
             AirEntities.GetContext().SaveChanges();
-           
-            button.Background = Brushes.Red;
-            
         }
-
     }
 }
